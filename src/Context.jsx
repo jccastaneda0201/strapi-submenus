8ce28f6f9ff417edfa8ec12302c2dbfcdd86109f
Juan Castaneda <jccastanedacamargo@outlook.com>
2025-07-18 16:15:47 +0200

    create new components and update navbar component

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,39 +1,23 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useState, useContext } from 'react';
 
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
+  const [pageId, setPageId] = useState(null);
   const openSidebar = () => {
     setIsSidebarOpen(true);
   };
   const closeSidebar = () => {
     setIsSidebarOpen(false);
   };
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
   return (
-    <AppContext.Provider
-      value={{
-        isSidebarOpen,
-        isModalOpen,
-        openSidebar,
-        closeSidebar,
-        openModal,
-        closeModal,
-      }}
-    >
+    <AppContext.Provider value={{ isSidebarOpen, openSidebar, closeSidebar, pageId, setPageId }}>
       {children}
     </AppContext.Provider>
   );
 };
+
 export const useGlobalContext = () => {
   return useContext(AppContext);
 };
